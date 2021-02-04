0a8da39237769ddf2e55399c6376be621ce5aa61
Mike Surcouf <mikes@surcouf.co.uk>
2021-02-04 18:17:04 +0000

    Add api and include wasm serving

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,14 @@
-﻿namespace BlazorRepl.Server
-{
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
+namespace Server
+{
     public class Program
     {
         public static void Main(string[] args)
@@ -11,9 +17,10 @@
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host
-                .CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(
-                    webBuilder => webBuilder.UseStartup<Startup>().UseSetting(WebHostDefaults.DetailedErrorsKey, bool.TrueString));
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
