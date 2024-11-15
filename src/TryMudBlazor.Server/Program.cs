34f7d446b278915e4527d5f95dd7cc0b999c37fa
Artyom M. <artem.melnikov@live.com>
2024-11-15 19:56:36 +0200

    Abandon Startup.cs (#159)

--- src/TryMudBlazor.Server/Program.cs
+++ src/TryMudBlazor.Server/Program.cs
@@ -1,16 +1,48 @@
+using MudBlazor.Examples.Data;
+
 namespace TryMudBlazor.Server;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddScoped<IPeriodicTableService, PeriodicTableService>();
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
             {
-                webBuilder.UseStartup<Startup>();
+                policy.WithOrigins("https://www.mudblazor.com", "https://mudblazor.com");
             });
+        });
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseCors();
+
+        // Needed for wasm project
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.MapControllers();
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
+    }
 }
