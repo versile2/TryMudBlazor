bc6101f03fd584563cea9bd81343d05fe24c775b
Versile Johnson II <versilej@gmail.com>
2024-12-23 21:37:14 -0600

    dateupdate

--- src/TryMudBlazor.Client/DateTimeVersion.cs
+++ src/TryMudBlazor.Client/DateTimeVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class DateTimeVersion
     {
-        public static string LatestVersion = "V: 8.xx as of 14:10 EST 12/20/24";
+        public static string LatestVersion = "V: 8.xx as of 22:30 EST 12/23/24";
 
     }
 }
