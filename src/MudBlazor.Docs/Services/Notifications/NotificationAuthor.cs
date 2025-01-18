d1a6b187f6ca503e00319e0e4829b65eedcb08f8
Versile Johnson II <versilej@gmail.com>
2025-01-17 20:25:24 -0600

    subtree

--- src/MudBlazor.Docs/Services/Notifications/NotificationAuthor.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace MudBlazor.Docs.Services.Notifications;
-
-public record NotificationAuthor(string DisplayName, string AvatarUrl);
--- src/MudBlazor/Interop/EventHandlers.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using Microsoft.AspNetCore.Components;
-
-namespace MudBlazor;
-
-// used in MudCollapse
-[EventHandler("ontransitionend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
-public static class EventHandlers;
